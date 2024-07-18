1c757a2a579cd7b0b7f7b32b5492764e2eca9921
Ashiiish3 <Ashiiish777@gmail.com>
2024-07-18 23:13:22 +0530

    update

--- My-Portfolio/src/Components/Home.jsx
+++ My-Portfolio/src/Components/Home.jsx
@@ -30,24 +30,24 @@ export default function Home() {
           <p className="mt-5 tracking-wide text-[17px] leading-6 text-textColor">I can  build the front-end portion of websites the part users see and interact with. and I can also create websites using web languages such as HTML, CSS, JavaScript and React.JS that allow users to access and interact with the site.</p>
           <div>
             <p className="uppercase text-textColor text-[14px] font-medium tracking-widest mt-14">You can find me</p>
-            <div className="flex gap-4">
-              <span className="BannerIcon shadow-boxShadowLightMode">
+            <div className="flex gap-4 mt-5">
+              <span className="BannerIcon">
                 <LuLinkedin />
               </span>
-              <span className="BannerIcon shadow-boxShadowLightMode">
+              <span className="BannerIcon">
                 <FaInstagram />
               </span>
-              <span className="BannerIcon shadow-boxShadowLightMode">
+              <span className="BannerIcon">
                 <FiTwitter />
               </span>
-              <span className="BannerIcon shadow-boxShadowLightMode">
+              <span className="BannerIcon">
                 <FiGithub />
               </span>
             </div>
           </div>
         </div>
-        <div className="w-[50%] bg-green-300 flex justify-center">
-          <div>
+        <div className="w-[50%] flex justify-center">
+          <div className="animateBox p-3 rounded-full h-[500px] w-[500px] animate-gradient bg-bannerBgColor">
             <img src={logo} alt={logo} className="brightness-125" />
           </div>
         </div>
--- My-Portfolio/tailwind.config.js
+++ My-Portfolio/tailwind.config.js
@@ -9,14 +9,20 @@ module.exports = {
         darkBodyColor: "#212428",
         lightBodyColor: "#ecf0f3",
         textColor: "#757575",
-        boxBg: "linear-gradient(145deg, #1e2024, #23272b)",
         designColor: "#ff014f",
         borderColor: "#d1d6db",
-        textDarkMode: "#c4cfde"
+        textDarkMode: "#c4cfde",
       },
       boxShadow: {
         boxShadowLightMode: "5px 5px 15px #D1D9E6, -5px -5px 15px #ffffff",
         boxShadowDarkMode: "5px 5px 16px #000000, -5px -5px 16px #545151"
+      },
+      backgroundImage: {
+        linearGradients : "linear-gradient(145deg, #6a67ce, #fc636b)",
+        bannerBgColor: "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)"
+      },
+      animation:{
+        gradient: "gradients 3s ease infinite"
       }
     },
   },
