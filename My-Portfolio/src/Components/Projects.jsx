1ceb4b624473d13bfda37b6cc750064f96402c6e
Ashiiish3 <Ashiiish777@gmail.com>
2024-07-23 23:03:55 +0530

    update

--- My-Portfolio/src/Components/Projects.jsx
+++ My-Portfolio/src/Components/Projects.jsx
@@ -1,34 +1,120 @@
-import React from "react";
-import {projectData} from '../Constant/index'
+import React, { useState } from "react";
+import "swiper/css";
+import { projectData } from "../Constant/index";
 import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import { Tooltip } from "react-tooltip";
+import { TbExternalLink } from "react-icons/tb";
+import { FaGithub } from "react-icons/fa6";
+import { Swiper, SwiperSlide } from "swiper/react";
+import WorkSliderBtns from "./WorkSliderBtns";
 
 export default function Projects() {
-    console.log(projectData)
+  const [project, setProject] = useState(projectData[0]);
+  const handleSlideChange = (swiper) => {
+    const currenInd = swiper.activeIndex;
+    setProject(projectData[currenInd]);
+  };
   return (
     <section className="bg-lightBodyColor">
-      <div className="max-w-[68%] m-auto py-24 bg-lightBodyColor border-b-gray-300 border-b-[1px]">
-        <p className="uppercase text-designColor tracking-widest font-medium">Projects</p>
+      <div className="max-w-[68%] m-auto py-24 border-b-gray-300 border-b-[1px]">
+        <p className="uppercase text-designColor tracking-widest font-medium">
+          Projects
+        </p>
         <h1 className="mt-1 text-6xl font-Montserratt font-[600] text-darkBodyColor">
           My Projects
         </h1>
-        <div className="mt-12">
-            {projectData.map((project)=>(
-                <div className="grid grid-cols-2 gap-8">
-                    <div className="text-left">
-                        <h1 className="number text-9xl font-bold text-transparent">{project.id}</h1>
-                        <h2>{project.title}</h2>
-                        <p>{project.description}</p>
-                        <p>{project.languageName}</p>
-                        <hr />
-                        <div>
-                            <NavLink>Project Link</NavLink>
-                            <NavLink>github link</NavLink>
-                        </div>
-                    </div>
-                    <div><img src={project.image} alt="" /></div>
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="mt-12 flex flex-col justify-center"
+        >
+          <div className="container mx-auto">
+            <div className="flex flex-col xl:flex-row gap-10">
+              <div className="w-full xl:w-[50%] text-start">
+                <h1 className="number text-8xl leading-none font-bold text-transparent">
+                  {project.number}
+                </h1>
+                <h3 className="mt-4 text-sm lg:text-3xl font-medium">
+                  {project.title}
+                </h3>
+                <p className="mt-3 font-medium text-md tracking-wide font-Montserratt text-textColor">
+                  {project.description}
+                </p>
+                <ul className="flex mt-4 text-designColor font-medium font-Montserratt tracking-wide gap-3">
+                  {project.language.map((ele, index) => {
+                    return (
+                      <li key={index}>
+                        {ele.name}{" "}
+                        {index !== project.language.length - 1 && ","}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <hr className="my-4 border-gray-400" />
+                <div className="flex gap-3">
+                  <div className="bg-lightBodyColor shadow-boxShadowLightMode p-3 rounded-full hover:bg-linearGradients hover:text-white">
+                    <NavLink
+                      className="project-link text-2xl"
+                      to={project.projectLink}
+                      target="_blank"
+                    >
+                      <span>
+                        <TbExternalLink />
+                      </span>
+                    </NavLink>
+                    <Tooltip anchorSelect=".project-link" place="top">
+                      Live Project
+                    </Tooltip>
+                  </div>
+                  <div className="bg-lightBodyColor shadow-boxShadowLightMode p-3 rounded-full hover:bg-linearGradients hover:text-white">
+                    <NavLink
+                      className="github-link text-2xl"
+                      to={project.githubLink}
+                      target="_blank"
+                    >
+                      <span>
+                        <FaGithub />
+                      </span>
+                    </NavLink>
+                    <Tooltip anchorSelect=".github-link" place="top">
+                      Github Link
+                    </Tooltip>
+                  </div>
                 </div>
-            ))}
-        </div>
+              </div>
+              <div className="w-full xl:w-[50%] bg-red-600">
+                <Swiper
+                  spaceBetween={30}
+                  slidesPerView={1}
+                  className="w-full bg-yellow-600"
+                  onSlideChange={handleSlideChange}
+                >
+                  <div className="flex flex-row w-full">
+                    {projectData.map((oneProject, index) => {
+                      return (
+                        <SwiperSlide key={index} className="w-full">
+                          <div className="w-full h-full group flex justify-center items-center">
+                            <img
+                              src={project.image}
+                              alt=""
+                              className="object-cover"
+                            />
+                          </div>
+                        </SwiperSlide>
+                      );
+                    })}
+                  </div>
+                  <WorkSliderBtns
+                    containerStyles="z-50 flex gap-5"
+                    btnStyles="bg-lightBodyColor shadow-boxShadowLightMode p-2 rounded-full"
+                    iconStyles="text-2xl text-designColor"
+                  />
+                </Swiper>
+              </div>
+            </div>
+          </div>
+        </motion.section>
       </div>
     </section>
   );
--- My-Portfolio/src/Constant/index.js
+++ My-Portfolio/src/Constant/index.js
@@ -6,6 +6,8 @@ import Bootstrap from '../Assets/Images/Bootstrap.png'
 import React from '../Assets/Images/React-js.png'
 import DeathWisCoffeehWebsite from '../Assets/Images/Death-wish-coffee-website-image.png'
 import FlipkartWebsite from '../Assets/Images/Flipkart-image.jpg'
+import PetdogWebsite from '../Assets/Images/Petdog-website-image.jpg'
+import HighTechWebsite from '../Assets/Images/HigtTech-image.jpg'
 
 export const skillsImagesData = [
     {
@@ -42,30 +44,39 @@ export const skillsImagesData = [
 
 export const projectData = [
     {
-        id: "01",
+        number: "01",
         title: "Death Wish Coffee Website",
         description: " This project is clone of Death wish coffee website which is a coffee brand based in the United States. Their coffee is primarily sold online, but can also be found in grocery stores across the United States.",
-        languageName: "HTML5, CSS3, Javascript, Bootstrap, JSON-Server",
+        language: [{name: "HTML 5"}, {name: "CSS 3"}, {name:"Javascript"},{name:"Bootstrap"},{name:"JSON-Server"}],
         image: DeathWisCoffeehWebsite,
         projectLink: "https://death-wish-coffee-website.vercel.app/",
         githubLink: "https://github.com/Ashiiish3/Death-Wish-Coffee-Clone"
     },
     {
-        id: "02",
+        number: "02",
         title: "Flipkart Clone",
         description: "This project is clone of Flipkart website India's biggest online store for Mobiles, Fashion (Clothes/Shoes), Electronics, Home Appliances, Books, Home, Furniture, Grocery, Jewelry, Sporting goods etc.",
-        languageName: "HTML5, CSS3, Javascript, Bootstrap, JSON-Server",
+        language: [{name: "HTML 5"}, {name: "CSS 3"}, {name:"Javascript"},{name:"JSON-Server"},{name:"Bootstrap"}],
         image: FlipkartWebsite,
         projectLink: "https://flipkart-clone-pi-vert.vercel.app/",
         githubLink: "https://github.com/Ashiiish3/Flipkart-Clone"
     },
     {
-        id: "03",
-        title: "Flipkart Clone",
-        description: "This project is clone of Flipkart website India's biggest online store for Mobiles, Fashion (Clothes/Shoes), Electronics, Home Appliances, Books, Home, Furniture, Grocery, Jewelry, Sporting goods etc.",
-        languageName: "HTML5, CSS3, Javascript, Bootstrap, JSON-Server",
-        image: FlipkartWebsite,
-        projectLink: "https://flipkart-clone-pi-vert.vercel.app/",
-        githubLink: "https://github.com/Ashiiish3/Flipkart-Clone"
+        number: "03",
+        title: "Veterinarian Website",
+        description: "A veterinarian website typically refers to an online platform that provides information, resources, and services related to veterinary medicine and animal care.",
+        language: [{name: "HTML 5"}, {name: "CSS 3"}, {name:"Javascript"},{name:"Bootstrap"}],
+        image: PetdogWebsite,
+        projectLink: "https://pethealth-website.000webhostapp.com/",
+        githubLink: "https://github.com/Ashiiish3/AS-Websites/tree/main/Dog%20Hospital%20Website"
+    },
+    {
+        number: "04",
+        title: "HighTech Website Clone",
+        description: "This project is a clone of HighTech Website that emphasizes the use of advanced technologies and modern design. This term is often associated with sectors that focus on technological innovation, including electronics, computers, and various scientific fields.",
+        language: [{name: "HTML 5"}, {name: "CSS 3"}, {name:"Javascript"},{name:"Bootstrap"},{name:"JQuery"}],
+        image: HighTechWebsite,
+        projectLink: "https://as-hightech.000webhostapp.com/",
+        githubLink: "https://github.com/Ashiiish3/AS-Websites/tree/main/HighTech%20Website%20Template"
     }
 ]
